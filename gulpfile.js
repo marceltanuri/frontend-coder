87799d2b9a4a34327d77e2c01c4150913a699652
Marcel Tanuri (Inetum) <marcel.tanuri@inetum.com>
2022-01-21 21:28:49 +0000

    format code

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,7 @@ const concatCss = require('gulp-concat-css');
 const ts = require('gulp-typescript');
 const fs = require('fs');
 
-
-gulp.task('compress', function() {
+gulp.task('compress', function () {
   return gulp.src(['src/*.js'])
     .pipe(minify())
     .pipe(gulp.dest('dist/js'))
@@ -16,61 +15,60 @@ gulp.task('compress', function() {
 
 gulp.task('transpile-header', function () {
   return gulp.src('src/js/header/**/*.ts')
-      .pipe(ts({
-          noImplicitAny: true,
-          outFile: 'main.js'
-      }))
-      .pipe(gulp.dest('dist/js/header'));
+    .pipe(ts({
+      noImplicitAny: true,
+      outFile: 'main.js'
+    }))
+    .pipe(gulp.dest('dist/js/header'));
 });
 
 gulp.task('transpile-footer', function () {
-    return gulp.src('src/js/footer/**/*.ts')
-        .pipe(ts({
-            noImplicitAny: true,
-            outFile: 'main.js'
-        }))
-        .pipe(gulp.dest('dist/js/footer'));
-  });
-
- 
-gulp.task('clean', function () {
-    return gulp.src('dist/*', {read: false})
-        .pipe(clean());
+  return gulp.src('src/js/footer/**/*.ts')
+    .pipe(ts({
+      noImplicitAny: true,
+      outFile: 'main.js'
+    }))
+    .pipe(gulp.dest('dist/js/footer'));
 });
 
-  gulp.task('generate-body', function() {   
-    return gulp.src(['src/template-body.html'])
-      .pipe(replace("<!--header-js-->", function(s) {
-        var _file = fs.readFileSync('dist/js/header/main.js', 'utf8');
-        return '<script>\n' + _file + '\n</script>';
-    })).pipe(replace("<!--footer-js-->", function(s) {
-        var _file = fs.readFileSync('dist/js/footer/main.js', 'utf8');
-        return '<script>\n' + _file + '\n</script>';
-    })).pipe(replace("<!--style-->", function(s) {
-        var _file = fs.readFileSync('dist/css/everything.css', 'utf8');
-        return '<style>\n' + _file + '\n</style>';
-    })).pipe(replace("<!--content-->", function(s) {
-        var _file = fs.readFileSync('src/content.html', 'utf8');
-        return _file;
-    }))
-    .pipe(replace("<!--imports-->", function(s) {
-        var _file = fs.readFileSync('src/imports.html', 'utf8');
-        return _file;
-    }))
-      .pipe(gulp.dest('dist/html/'))
-  });
+gulp.task('clean', function () {
+  return gulp.src('dist/*', { read: false })
+    .pipe(clean());
+});
 
-  gulp.task('generate-final', function() {   
-    return gulp.src(['src/template.html'])
-      .pipe(replace("<!--body-->", function(s) {
-        var _file = fs.readFileSync('dist/html/template-body.html', 'utf8');
-        return '<body>\n' + _file + '\n</body>';
+gulp.task('generate-body', function () {
+  return gulp.src(['src/template-body.html'])
+    .pipe(replace("<!--header-js-->", function (s) {
+      var _file = fs.readFileSync('dist/js/header/main.js', 'utf8');
+      return '<script>\n' + _file + '\n</script>';
+    })).pipe(replace("<!--footer-js-->", function (s) {
+      var _file = fs.readFileSync('dist/js/footer/main.js', 'utf8');
+      return '<script>\n' + _file + '\n</script>';
+    })).pipe(replace("<!--style-->", function (s) {
+      var _file = fs.readFileSync('dist/css/everything.css', 'utf8');
+      return '<style>\n' + _file + '\n</style>';
+    })).pipe(replace("<!--content-->", function (s) {
+      var _file = fs.readFileSync('src/content.html', 'utf8');
+      return _file;
+    }))
+    .pipe(replace("<!--imports-->", function (s) {
+      var _file = fs.readFileSync('src/imports.html', 'utf8');
+      return _file;
+    }))
+    .pipe(gulp.dest('dist/html/'))
+});
+
+gulp.task('generate-final', function () {
+  return gulp.src(['src/template.html'])
+    .pipe(replace("<!--body-->", function (s) {
+      var _file = fs.readFileSync('dist/html/template-body.html', 'utf8');
+      return '<body>\n' + _file + '\n</body>';
     })).pipe(gulp.dest('dist/html/'))
-  });
+});
 
-  gulp.task('watch', function(){
-    gulp.watch('src/**/*.*', gulp.series('build'));
-  })
+gulp.task('watch', function () {
+  gulp.watch('src/**/*.*', gulp.series('build'));
+})
 
 gulp.task('minify', function () {
   return gulp.src('src/css/*.css')
